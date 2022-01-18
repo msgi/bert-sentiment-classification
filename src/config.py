4f1661d14ab39f2f4a578947bf18b9a43ca63174
msgi <mayuan120226@163.com>
2022-01-18 17:08:14 +0800

    init

---
+++ src/config.py
@@ -0,0 +1,18 @@
+import transformers
+import torch
+
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 10
+BERT_PATH = "../input/bert_base_uncased/"
+# BERT_PATH = "../input/bert_base_chinese/"
+MODEL_PATH = "model.bin"
+TRAINING_FILE = "../input/sentiment/imdb.csv"
+# TRAINING_FILE = "../input/sentiment/sentiment.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BERT_PATH,
+    do_lower_case=True
+)
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
